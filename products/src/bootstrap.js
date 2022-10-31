8cee7bd90d1236b8234447ab79e69e5599c34ee4
Jac Desktop <jlg17@sfu.ca>
2022-10-30 18:28:40 -0700

    30: end of Section 3

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,31 @@
 import faker from 'faker';
 
-let products = '';
-
-for (let i = 0; i < 5; i++)
+//startpoint of app
+const mount = (el) => 
 {
-    const name = faker.commerce.productName();
-    products += `<div>${name}</div>`
+    let products = '';
+
+    for (let i = 0; i < 5; i++)
+    {
+        const name = faker.commerce.productName();
+        products += `<div>${name}</div>`
+    }
+    el.innerHTML = products;
+};
+
+// context 1 - running file in dev in isolation
+    //use local index.html -> has div with known id, immediately render
+if (process.env.NODE_ENV === 'development')
+{
+    const el = document.querySelector('#dev-products');
+    //assuming container does not contain this id ^
+    if (el)
+    {
+        mount(el);
+    }
+
 }
-document.querySelector('#dev-products').innerHTML = products;
+
+//context 2 - running file in dev or prod through container
+    //no guarantee that will be div with known id, don't immediately render
+export { mount };
